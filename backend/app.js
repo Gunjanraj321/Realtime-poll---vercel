cef16b8f88ec029776b8352913538349be34d6d6
Gunjan Kumar <118360371+Gunjanraj321@users.noreply.github.com>
2024-06-24 12:50:56 +0530

    some update

--- backend/app.js
+++ backend/app.js
@@ -30,9 +30,9 @@ app.use(commentRoutes);
 //----------Deployement---------
 const __dirname1 = path.resolve();
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname1, '../frontend/build')));
+    app.use(express.static(path.join(__dirname1, '../frontend/dist')));
     app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname1, 'frontend', 'public', 'index.html'));
+        res.sendFile(path.resolve(__dirname1, 'frontend', 'dist', 'index.html'));
         });
 }else{
     app.get('/', (req, res) => {
